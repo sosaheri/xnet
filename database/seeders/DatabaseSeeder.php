dc6a6f68f4addc9f508a60b5b6b2febbd0a73ac5
Heriberto <sosaheriberto2001@gmail.com>
2023-09-28 17:34:47 -0400

    Agregar seeder para usuario de prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Department;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,9 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
         $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
         $this->call(DepartmentSeeder::class);
+        
+        User::create([
+            'name' => 'user',
+            'email' => 'user@xnet.com',
+            'password' => Hash::make('password'),
+            'role_id' => Role::ROLE_EMPLEADO,
+            'department_id' => Department::DEPT_ATC,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
